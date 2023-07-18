9b5c139e689084a25d5d80b160843f8b5a527077
BradFernando <bfcorro@espe.edu.ec>
2023-07-18 16:49:12 -0500

    carrito

--- ProyectoBitsBites/Controllers/CarritoController.cs
+++ ProyectoBitsBites/Controllers/CarritoController.cs
@@ -9,77 +9,33 @@ using System.Web.Mvc;
 namespace ProyectoBitsBites.Controllers
 {
     public class CarritoController : Controller
+{
+    private static List<Productos> carrito = new List<Productos>();
+
+    public ActionResult AgregarProducto(int id)
     {
-        private RestauranteBitsBitesEntities1 db = new RestauranteBitsBitesEntities1();
-
-        public ActionResult Carrito()
+        // Lógica para agregar el producto al carrito
+        using (var db = new RestauranteBitsBitesEntities1())
         {
-            // Obtener los datos de las tablas Pedidos y DetallesPedido
-            List<Pedidos> pedidos = db.Pedidos.ToList();
-            List<DetallesPedido> detalles = db.DetallesPedido.ToList();
-
-            // Combinar los datos de las tablas Pedidos y DetallesPedido en objetos CarritoItem
-            List<CarritoItem> carritoItems = new List<CarritoItem>();
-
-            foreach (var pedido in pedidos)
+            var producto = db.Productos.FirstOrDefault(p => p.id_producto == id);
+            if (producto != null)
             {
-                var detalle = detalles.FirstOrDefault(d => d.id_pedido == pedido.id_pedido);
-
-                if (detalle != null)
-                {
-                    var carritoItem = new CarritoItem
-                    {
-                        ID_Pedido = pedido.id_pedido,
-                        ID_Cliente = pedido.id_cliente,
-                        Fecha_Pedido = pedido.fecha_pedido,
-                        Estado_Pedido = pedido.estado_pedido,
-                        ID_Detalle = detalle.id_detalle,
-                        ID_Producto = detalle.id_producto,
-                        Cantidad = detalle.cantidad,
-                        Precio = detalle.subtotal
-                    };
-
-                    carritoItems.Add(carritoItem);
-                }
+                carrito.Add(producto);
             }
-
-            // Pasar la lista de objetos CarritoItem a la vista
-            return View(carritoItems);
         }
 
-        public ActionResult AgregarAlCarrito(int id)
+        return RedirectToAction("ProductHamburguesas", "Product");
+    }
+
+        public ActionResult MostrarCarrito()
         {
-            // Lógica para agregar un producto al carrito
+            int cantidadProductos = carrito.Count;
 
-            // ... 
+            ViewBag.CantidadProductosCarrito = cantidadProductos;
+            ViewBag.ImagenCarrito = "https://cdn-icons-png.flaticon.com/512/107/107831.png?w=740&t=st=1689597510~exp=1689598110~hmac=0f0d8907415e059249a0c16e998d643e3960eaeef34f98353d93589532b3356f";
 
-
-            // Actualizar el contador del carrito en el ViewBag
-            ViewBag.CartCount = ObtenerContadorDelCarrito();
-
-            // Redirigir al usuario a la página de productos
-            return RedirectToAction("ProductHamburguesas", "Product");
+            return PartialView("_CarritoIcono");
         }
 
-        public ActionResult EliminarDelCarrito(int id)
-        {
-            // Lógica para eliminar un producto del carrito
-            // ...
-
-            // Actualizar el contador del carrito en el ViewBag
-            ViewBag.CartCount = ObtenerContadorDelCarrito();
-
-            // Redirigir al usuario a la página del carrito
-            return RedirectToAction("Carrito");
-        }
-
-        private int ObtenerContadorDelCarrito()
-        {
-            // Lógica para obtener el número de elementos en el carrito
-            // ...
-
-            // Aquí devolvemos un valor de ejemplo, reemplázalo con la lógica real
-            return 0;
-        }
     }
 }
